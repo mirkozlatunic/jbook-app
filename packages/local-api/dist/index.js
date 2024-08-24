91d4c30fe845efa13944d79db469522b0a4882f9
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-08-23 18:10:57 -0700

    passing paths into CLI

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
 const serve = (port, filename, dir) => {
-    console.log("serviing traffic on port", port);
+    console.log("serving traffic on port", port);
     console.log("saving/fetching cell from", filename);
     console.log("that file is in dir", dir);
 };
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,5 +1,5 @@
 export const serve = (port: number, filename: string, dir: string) => {
-  console.log("serviing traffic on port", port);
+  console.log("serving traffic on port", port);
   console.log("saving/fetching cell from", filename);
   console.log("that file is in dir", dir);
 };
