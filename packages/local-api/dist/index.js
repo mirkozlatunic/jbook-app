c8a214b815f7d5dc82962ce4bdf1ac1dd65ef151
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-08-23 19:15:03 -0700

    create build for local-client

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -5,10 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
 const express_1 = __importDefault(require("express"));
+const http_proxy_middleware_1 = require("http-proxy-middleware");
 const serve = (port, filename, dir) => {
     const app = (0, express_1.default)();
-    app.listen(port, () => {
-        console.log("Listening on port ", port);
+    app.use((0, http_proxy_middleware_1.createProxyMiddleware)({
+        target: "http://localhost:3000",
+        ws: true,
+        logLevel: "silent",
+    }));
+    return new Promise((resolve, reject) => {
+        app.listen(port, resolve).on("error", reject);
     });
 };
 exports.serve = serve;
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,9 +1,18 @@
 import express from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 export const serve = (port: number, filename: string, dir: string) => {
   const app = express();
 
-  app.listen(port, () => {
-    console.log("Listening on port ", port);
+  app.use(
+    createProxyMiddleware({
+      target: "http://localhost:3000",
+      ws: true,
+      logLevel: "silent",
+    })
+  );
+
+  return new Promise<void>((resolve, reject) => {
+    app.listen(port, resolve).on("error", reject);
   });
 };
